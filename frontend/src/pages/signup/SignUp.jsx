4791b079b66c22dfd6ec98eed8151a64e40221a2
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2024-02-23 21:44:40 +0530

    signup

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -95,8 +95,15 @@ const signUp = () => {
             Already hace an account?
           </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700 ">
-              Sign Up
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700 "
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>
