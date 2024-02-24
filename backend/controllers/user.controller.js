4b8ad2a91b7bbfeb921414832480f6c826a73238
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2024-02-24 23:44:18 +0530

    changes 24feb

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,9 +4,9 @@ export const getUsersForSidebar = async (req, res) => {
     const loggedInUserId = req.user._id; // Get the id of the user who is currently logged in
     const filteredUsers = await User.find({
       _id: { $ne: loggedInUserId },
-    }).select("-password"); // Get all the users except the logged in user and if you want to send message yourself then remove this condition _id: { $ne: loggedInUserId } from the find query and also remove the select("-password") to get the password of the user
+    }).select("-password");
 
-    res.status(200).json({ filteredUsers });
+    res.status(200).json(filteredUsers); // Get all the users except the logged in user and if you want to send message yourself then remove this condition _id: { $ne: loggedInUserId } from the find query and also remove the select("-password") to get the password of the user
   } catch (error) {
     console.log("Error in getUserForSidebar: ", error.message);
     res.status(500).json({ error: "Internal Server Error" });
--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,18 +1,38 @@
 import { BsSend } from "react-icons/bs";
+import { useState } from "react";
+import useSendMessage from "../../hooks/useSendMessage";
+
 const MessageInput = () => {
+  const [message, setMessage] = useState("");
+  const { loading, sendMessage } = useSendMessage();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Message sent");
+    if (!message) return;
+    await sendMessage(message);
+    setMessage("");
+  };
+
   return (
-    <form className="px-4 my-3 ">
+    <form className="px-4 my-3 " onSubmit={handleSubmit}>
       <div className="w-full relative">
         <input
           type="text"
           className="border text-sm block text-white w-full bg-gray-700 rounded-lg p-2.5 border-gray-600"
           placeholder="Type a message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button
           type="submit"
           className="absolute inset-y-0 end-0 flex items-center pe-3 text-gray-500"
         >
-          <BsSend />
+          {loading ? (
+            <div className="loading loading-spinner"></div>
+          ) : (
+            <BsSend />
+          )}
         </button>
       </div>
     </form>
